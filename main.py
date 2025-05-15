a121a52f4e2c63a1421d5a2f94664e3ece167e10
OscarJEB <burgeo1@macstudent.com.au>
2025-05-15 00:05:13 +0000

    Hello Mz Tor I have made the novel child class

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
-Create the Book class (plus methods and attributes)
-Create the Novel class that inherits from Book class.
-Create the Magazine class that inherits from Book class.
+#Write Python code for both the Novel and Magazine classes modelled in the previous slide. Include a suitable constructor method which uses the Book constructor method. Instantiate 2 novels and 2 magazines and print their details.
+#Create the Book class (plus methods and attributes)
+class Book:
+    def __init__(self, numPages, author, title):
+        self.numPages = numPages
+        self.author = author
+        self.title = title
+
+class Novel(Book):
+    def __init__(self, numPages, author, title, genre, numChaps):
+        super().__init__(numPages, author, title)
+        self.genre = genre
+        self.numChaps = numChaps
+
+
+
+#Create the Novel class that inherits from Book class.
+#Create the Magazine class that inherits from Book class.
